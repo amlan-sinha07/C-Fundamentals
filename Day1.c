285221d3953d6b8de9286bfe52581c599c4c3ac6
Amlan Sinha <amlansinha386@gmail.com>
2026-08-01 20:40:24 +0530

    Add initial implementation of basic sum calculator

---
+++ Day1.c
@@ -0,0 +1,24 @@
+//day 62 originally but i start from day 1 what a nonsense but i will track day record 
+//to show my children that their dad is goated as ronaldo 
+#include <stdio.h>
+
+int main() {
+    int a;
+    int b;
+
+
+    printf("enter number a: ");
+    scanf("%d",&a);
+    
+    
+    printf("enter number b: ");
+    scanf("%d",&b);
+    
+    
+    int sum=a+b;
+    printf("sum is: %d\n",sum);
+
+    printf("life is going on\n");
+    
+    return 0;
+}
