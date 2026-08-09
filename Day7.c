0f1d4dd30e1b6a1f496f45fe45c3b7e3ba2b58be
Amlan Sinha <amlansinha386@gmail.com>
2026-08-09 11:07:24 +0530

    Add Day 70 implementation with integer and float calculations

---
+++ Day7.c
@@ -0,0 +1,20 @@
+//Day 70
+#include <stdio.h>
+#include <math.h>
+int main(){
+    int a=(int)1.999999;
+    printf("%d\n",a+1);
+    
+    //int x=(4+9)*10;
+    //printf("%d\n",x);
+    
+    
+    //associativity (for same precedence)
+    // left to right 
+    //double x=4*3/6*2;
+
+
+    float x=5+2/2*3;
+    printf("%f\n",x);
+    return 0;
+}
